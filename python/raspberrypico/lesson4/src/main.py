10d3f120355726443c96a33ae750d1f0bc692613
tdeknecht <tdeknecht@gmail.com>
2022-11-20 12:17:04 -0600

    refactor: changing lesson 4 to include a driver

---
+++ python/raspberrypico/lesson4/src/main.py
@@ -0,0 +1,24 @@
+from machine import Pin, UART
+
+from drivers import gtu7
+
+if __name__ == "__main__" :
+
+    uart = UART(1,
+                baudrate=9600,
+                tx=Pin(4),
+                rx=Pin(5))
+
+    gps_module = gtu7.GTU7(uart)
+    
+    while True :
+        try :
+            gps_data = gps_module.get_gps_data()
+            
+            print("Latitude: \t %d" % gps_data[0])
+            print("Longitude: \t %d" % gps_data[1])
+            print("Satellites: \t %d" % gps_data[2])
+            print("Time:\t\t %d \n" % gps_data[3])
+            
+        except Exception as e :
+            print("No GPS data returned.")
