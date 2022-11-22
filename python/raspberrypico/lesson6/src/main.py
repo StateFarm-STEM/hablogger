d47da84f8026fbf5e170c17cb56026b09a7951f2
tdeknecht <tdeknecht@gmail.com>
2022-11-21 21:01:04 -0600

    feat: brought in the updated gps driver and added logic to write its data to sd card

--- python/raspberrypico/lesson6/src/main.py
+++ python/raspberrypico/lesson6/src/main.py
@@ -1,7 +1,7 @@
-from machine import Pin, SPI, Timer, I2C
+from machine import Pin, SPI, Timer, I2C, UART
 from drivers import sdcard
+from drivers import gtu7 as gpsdriver
 from drivers import bmp180 as bmp180driver
-from time import sleep, gmtime
 
 import os
 
@@ -15,7 +15,8 @@ def initBmp180() :
                scl=Pin(17),
                sda=Pin(16),
                freq=100000)
-    
+
+
     # Initialize BMP180 with previously defined I2C config
     bmp180 = bmp180driver.BMP180(bus)
     
@@ -24,12 +25,18 @@ def initBmp180() :
 
     return bmp180
 
-def getDataBmp180(bmp180) :
-    temp = bmp180.temperature  # Capture temperature, assign to `temp` variable
-    p = bmp180.pressure        # Capture pressure, assign to `p` variable
-    altitude = bmp180.altitude # Capture altitude, assign to `altitude` variable
 
-    return [temp, p, altitude]
+def initGtu7() :
+    uart = UART(1,
+                baudrate=9600,
+                timeout=3600,
+                tx=Pin(4),
+                rx=Pin(5))
+
+    gtu7 = gpsdriver.GTU7(uart)
+
+    return gtu7
+
 
 def initSDCard(sd_dir) :
     spi = machine.SPI(1,
@@ -42,15 +49,18 @@ def initSDCard(sd_dir) :
                       mosi=machine.Pin(11), # Pico GPIO Pin 11
                       miso=machine.Pin(12)) # Pico GPIO Pin 12
     sd=sdcard.SDCard(spi,Pin(13))
-
-    vfs=os.VfsFat(sd)
      
     os.mount(sd,sd_dir)
 
-def writeCsvToSDCard(sd_dir, file_name, dataset) :
+
+def writeCsvToSDCard(sd_dir, file_name, dataset, header=None) :
     with open(sd_dir + '/' + file_name,'a+') as csv_out :
+        if header :
+            dataset = header
+            
         for data in dataset :
             if data == dataset[-1] :
+                # If last element in the dataset, don't add a comma.
                 csv_out.write('"'+str(data)+'"')
             else :
                 csv_out.write('"'+str(data)+'",')
@@ -76,23 +86,41 @@ def cleanupDirSDCard(rmdir) :
 
 if __name__ == "__main__" :   
     led = Pin(25, Pin.OUT)  # Assign on board LED to variable
-    sd_dir = '/sd'          # Directory created on SD card at root '/'. Expected format is '/<string>'. Example: '/sd'
-        
     machine.Timer().init(freq=1, mode=Timer.PERIODIC, callback=blinkLed)
-
+    
+    sd_dir = '/sd'          # Directory created on SD card at root '/'. Expected format is '/<string>'. Example: '/sd'
     initSDCard(sd_dir)
-    bmp180 = initBmp180()
+    
+    bmp180 = initBmp180()   # Initialize the BMP_180 (Temp/Pressure module)
+    gtu7 = initGtu7()       # Initialize the GT-U7 (GPS module)
+    
+    cleanupDirSDCard(sd_dir) # This helper function cleans up the SD card. Uncomment this to delete SD card data.
 
-    bmp_data = getDataBmp180(bmp180)
-
-    # print(bmp_data)
-
-    writeCsvToSDCard(sd_dir, "data.csv", bmp_data)
+    csv_header = [
+        "date",
+        "time",
+        "latitude",
+        "longitude",
+        "velocity",
+        "numSatellites",
+        "temperature",
+        "pressure",
+        "altitude",
+        ]
+    writeCsvToSDCard(sd_dir, "data.csv", None, csv_header) # Initialize CSV header
+    
+    for x in range(10) :
+        temp = bmp180.temperature  # Capture temperature, assign to `temp` variable
+        p = bmp180.pressure        # Capture pressure, assign to `p` variable
+        altitude = bmp180.altitude # Capture altitude, assign to `altitude` variable
+        
+        gpgga = gtu7.gpgga() # Capture NMEA GPGGA GPS data (http://aprs.gids.nl/nmea/#gga)
+        gprmc = gtu7.gprmc() # Capture NMEA GPRMC GPS data (http://aprs.gids.nl/nmea/#rmc)
+        
+        data = [gprmc[0], gprmc[1], gprmc[2], gprmc[3], gprmc[4], gpgga[3], temp, p, altitude]
+        
+        writeCsvToSDCard(sd_dir, "data.csv", data)
     
     # Read all the rows from the CSV and print them to the console.
     for row in readCsvFromSDCard(sd_dir, "data.csv") :
         print(row)
-
-    cleanupDirSDCard(sd_dir)
-    
-
