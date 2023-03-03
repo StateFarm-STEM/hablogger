2cb7bbd46e81bdb3f2f98bb4e0fdc34d6c053a8f
tdeknecht <tdeknecht@gmail.com>
2023-03-02 19:28:41 -0600

    refactor: removing LED entirely

--- python/raspberrypico/lesson-4/src/main.py
+++ python/raspberrypico/lesson-4/src/main.py
@@ -1,13 +1,9 @@
 # Import all needed libraries
-from machine import Pin, UART
+from machine import UART
 import time
 from drivers import gtu7
 
 if __name__ == "__main__" :
-    
-    led = Pin( #Initialize a variable called "led" as a "Pin" object
-    25, #Assign pin number for onboard led (25 for Pico and "LED" for PicoW)
-    Pin.OUT) #Specify this is an output pin, meaning we will send data to the pin vs recieving data from it
     
     # Define our UART(Universal Asynchronous Receiver/Transmitter)
     uart = UART(1,
@@ -18,8 +14,6 @@ if __name__ == "__main__" :
     
     # Loop for GPS coordinates and toggle the LED for action
     while True:
-        led.on()
-        
         gps_module = gtu7.GTU7(uart)
 
         gpgga_data = gps_module.gpgga()
@@ -27,5 +21,4 @@ if __name__ == "__main__" :
         print("Latitude:\t{0}\nLongitude:\t{1}\nSatellites:\t{2}\nTime:\t\t{3}\n".
               format(gpgga_data[1], gpgga_data[2], gpgga_data[3], gpgga_data[0]))
 
-        led.off()
         time.sleep(5)
